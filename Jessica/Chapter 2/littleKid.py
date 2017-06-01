5f86ebd2265b8b844f7193b11a4f4df08abb7e84
jreitz43 <jreitz43@gmail.com>
2017-05-31 21:48:44 -0600

    Chapter 2 collection
    Collection of Chapter 2 files going over flow controls

---
+++ Jessica/Chapter 2/littleKid.py
@@ -0,0 +1,8 @@
+name = 'Jess'
+age = 24
+if name == 'Jernaynay':
+    print('What do you want, Jernaynay?')
+elif age < 12:
+    print('Get out of here, kid.')
+else:
+    print('If this isn\'t Jernaynay or a little human, why are you here?')
---
+++ Jessica/Chapter 2/vampire2.py
@@ -0,0 +1,11 @@
+name = 'Dracula'
+age = 4000
+if name == 'Jess':
+    print('Hey Jess.')
+elif age < 12:
+    print('You must not enter the kingdom of doom.')
+elif age > 100:
+    print('No, just no.')
+elif age > 2000:
+    print('Access denied immortal jellyfish.')
+
