2c2724623fce234ab4b1ed10444328933baab936
Blake <blakekelley127@gmail.com>
2017-06-28 22:08:23 -0600

    Renamed chapter folders in order to make them easier to reference

--- Blake/Chapter 8/pprintModule.py
+++
@@ -1,7 +0,0 @@
-motorbikes = [{'color': 'blue', 'name': 'Yamaha'},
- {'color': 'red', 'name': 'Honda'},
- {'color': 'green', 'name': 'Kawasaki'},
- {'color': 'yellow', 'name': 'Suzuki'},
- {'color': 'orange', 'name': 'KTM'},
- {'color': 'white', 'name': 'Husqvarna'},
- {'color': 'red', 'name': 'Beta'}]
