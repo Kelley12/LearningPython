7da98570b4b02eae704abee3abb977ba44be38f2
Blake <blakekelley127@gmail.com>
2017-07-03 22:12:36 -0600

    Removing old folders
    Removing old renamed folders and files

--- Blake/Data Science/pyplot.py
+++ Blake/Data Science/pyplot.py
@@ -9,10 +9,21 @@ pop = [2.538,2.57,2.62,2.84,2.98,3.23,3.54]
 
 # Create a plot of the data
 plt.plot(year, pop)
+
+# Add title and axis labels
+plt.xlabel("Year")
+plt.ylabel("Population [million]")
+plt.title("Population over year")
+
 plt.show()
 plt.clf()
 
 # Create a scatter plot of the data
 plt.scatter(year, pop)
+plt.show()
+plt.clf()
+
+# Create a scatter plot of the data
+plt.hist(year, )
 plt.show()
 plt.clf()
