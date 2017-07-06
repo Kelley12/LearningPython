8c2203284d71cc0e6384e4c3ce07c677de7fdbd2
Blake <blakekelley127@gmail.com>
2017-07-05 19:52:15 -0600

    Finished Chapter 15
    Completed chapter 15, learned multi threading in Python, awesome that it
    has it.

---
+++ Blake/Chapter 15 - Time, Scheduling, and Launching Programs/threadDemo.py
@@ -0,0 +1,15 @@
+#! python3
+# threadDemo.py - A demo program in multi-threading
+
+import threading, time
+
+print('Start of program.')
+
+def takeANap():
+    time.sleep(5)
+    print("Wake up!")
+
+threadObj = threading.Thread(target=takeANap)
+threadObj.start()
+
+print("End of program.")
